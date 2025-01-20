0fa8462ae2e57ee07dfa2fdc64a0551af2897d37
SakharovIvan <i731901@gmail.com>
2025-01-20 22:11:02 +0300

    bd creat

--- utils/createPNGfrompdfBD.js
+++ utils/createPNGfrompdfBD.js
@@ -6,7 +6,7 @@ try {
   const __filename = process.cwd();
   const __dirname = path.dirname(__filename);
   const pdfPath = `${__dirname}/Scheme_service_server`;
-  async function createMasPictures(limit = 65, offset = 60) {
+  async function createMasPictures(limit = 70, offset = 65) {
     const pdf_paths_from_DB = await ToolPaths.findAll({
       raw: true,
       limit,
