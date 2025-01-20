7740c82281e50f82aaab2bc621281bb58443e346
SakharovIvan <i731901@gmail.com>
2025-01-20 22:53:29 +0300

    bd creat

--- utils/createPNGfrompdfBD.js
+++ utils/createPNGfrompdfBD.js
@@ -3,33 +3,25 @@ import pdfScheme_service from "../services/pdfScheme_service.js";
 import path from "path";
 
 const __filename = process.cwd();
-  const __dirname = path.dirname(__filename);
-  const pdfPath = `${__dirname}/Scheme_service_server`;
-  async function createMasPictures(limit = 70, offset = 68) {
-    const pdf_paths_from_DB = await ToolPaths.findAll({
-      raw: true,
-      limit,
-      offset,
-    });
-    const promises = pdf_paths_from_DB.map(
-      async ({ tool_code, tool_path }) =>
-        await pdfScheme_service.createJPGfromPDF(pdfPath + tool_path, tool_code)
-      //await pdfScheme_service.createPNGfromPDF(pdfPath + tool_path, tool_code);
-    );
-    return Promise.all(promises).then(() =>
-      console.log(`${limit} ${offset} pictures created`)
-    );
-  }
-try {
-  
-  let opt = 75;
-  setInterval(
-    () =>
-      createMasPictures(opt, opt - 5).then(() => {
-        opt += 5;
-      }),
-    5000
+const __dirname = path.dirname(__filename);
+const pdfPath = `${__dirname}/Scheme_service_server`;
+async function createMasPictures(limit = 70, offset = 68) {
+  const pdf_paths_from_DB = await ToolPaths.findAll({
+    raw: true,
+    limit,
+    offset,
+  });
+  const promises = pdf_paths_from_DB.map(
+    async ({ tool_code, tool_path }) =>
+      await pdfScheme_service.createJPGfromPDF(pdfPath + tool_path, tool_code)
+    //await pdfScheme_service.createPNGfromPDF(pdfPath + tool_path, tool_code);
   );
+  return Promise.all(promises).then(() =>
+    console.log(`${limit} ${offset} pictures created`)
+  );
+}
+try {
+  await createMasPictures(100, 70);
 } catch (err) {
   console.log(err);
 }
