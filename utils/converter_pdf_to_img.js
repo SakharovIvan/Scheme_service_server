a0f18ab9bddc77e37bd2bc72cb2a676719f6e56a
SakharovIvan <i731901@gmail.com>
2026-05-07 17:14:50 +0300

    r

--- utils/converter_pdf_to_img.js
+++ utils/converter_pdf_to_img.js
@@ -4,21 +4,16 @@ import { pngPath, jpgPath, __filename } from "../folders.js";
 
 async function pdftopngConvertor(path_to_pdf, tool_code, num = 1) {
   const document = await pdf(path_to_pdf, { scale: 3 });
-  console.log(path_to_pdf, tool_code, num,document.length 
-    ,document.length - num + 1 )
+
   const page12buffer = await document.getPage(document.length - num + 1);
-  console.log(`${pngPath}${num}_${tool_code}.png`)
   await fs.writeFile(`${pngPath}${num}_${tool_code}.png`, page12buffer);
-  console.log('png converted')
   return
 }
 
 async function pdftojpgConvertor(path_to_pdf, tool_code, num = 1) {
-    console.log(path_to_pdf,'3')
 
   const document = await pdf(path_to_pdf, { scale: 3 });
   const page12buffer = await document.getPage(document.length  - num + 1);
-  console.log(document.length + num - 1,'4')
 
   await fs.writeFile(`${jpgPath}${num}_${tool_code}.jpg`, page12buffer);
   return
